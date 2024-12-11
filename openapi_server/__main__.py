f24da45ee6bf0744b3675edb2ee9f9b30b730070
jesuchito <jcarreterol2002@gmail.com>
2024-12-11 16:49:57 +0100

    error usuario postgres

--- openapi_server/__main__.py
+++ openapi_server/__main__.py
@@ -20,7 +20,7 @@ app.add_api('openapi.yaml',
 #app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345@localhost:5432/Usuarios'
 
 "Comentar la conexión que no van a ocupar luego y, al hacer push al repositorio, no comentarizar esta conexión."
-app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:api_password@db:5432/api_database'
+app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:api_password@db:5432/api_database'
 
 app.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #esto es para que no se caiga a las 10 requests
